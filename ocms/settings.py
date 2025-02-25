87edbe39878617560626b18b807caa1c6ac5edae
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-02-25 14:03:56 +0300

    added workflows

--- ocms/settings.py
+++ ocms/settings.py
@@ -68,8 +68,13 @@ WSGI_APPLICATION = "ocms.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ocms",
+        "USER": "user",
+        "PASSWORD": "password",
+        "HOST": "db",
+        "PORT": '5432',
+
     }
 }
 
