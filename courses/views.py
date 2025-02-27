31f94251fcdb430dbed2f1d7cec07059ec4a874c
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-02-28 00:19:36 +0300

    added pagination to the course class and custom actions to the enrollment and course classes

--- courses/views.py
+++ courses/views.py
@@ -4,10 +4,27 @@ from .models import Course, Module, Enrollment
 from .serializers import CourseSerializer, ModuleSerializer, EnrollmentSerializer
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import action
+
+class CoursePagination(PageNumberPagination):
+    page_size = 10
+
 class CourseViewset(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = CoursePagination
+
+
+    @action(detail=True, methods=['get'])
+    def modules(self, request, pk=None):
+        course = self.get_object()
+        modules = Module.objects.filter(course=course)
+        serializer = ModuleSerializer(modules, many=True)
+        return Response(serializer.data)    
+
+
 
 class ModuleViewSet(viewsets.ModelViewSet):
     queryset = Module.objects.all()
@@ -19,6 +36,21 @@ class EnrollmentViewSet(viewsets.ModelViewSet):
     serializer_class = EnrollmentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    
+
+    @action(detail=True, methods=['post'])
+    def enroll_in_course(self, request, pk=None):
+        course = self.get_object()
+        user = request.user
+
+        # check if student is enrolled
+        if Enrollment.objects.filter(course=course, user=user).exists():
+            return Response ({"detail": "User is already enrolled in this course"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        enrollment = Enrollment.objects.create(course=course, user=user)
+        return Response(EnrollmentSerializer(enrollment).data, status=status.HTTP_201_CREATED)
+
+
+
+
 
 
